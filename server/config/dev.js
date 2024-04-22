c15a21c222a903bf4cc9b8f101e08231b9dfef99
mayankyadav1711 <mykyadav2003@gmail.com>
2024-04-22 21:09:08 +0530

    final

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
     MONGOURI: "mongodb+srv://myschemedb:I0BDqkdMyjUGzHK0@cluster0.dpry5c6.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET:"ioasdhfiodsahiod",
     EMAIL:"http://localhost:3000",
-    GPASS:"ahvqmnrzxnonrwf"
+    GPASS:"plnaaxvinyhrgeuz"
 }
 
 
